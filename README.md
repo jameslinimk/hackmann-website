585da4adf98cfd6f67494a7517ff97584b7e9b91
Jameslinimk <jameskimimk@gmail.com>
2024-01-10 23:50:59 -0500

    qwe

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Hackmann 2023 Website
+
+## Development
+
+```bash
+# If you don't have pnpm installed
+npm install -g pnpm
+
+# Install dependencies
+pnpm install
+
+# Run dev server
+pnpm run dev
+```
--- prisma/migrations/20240111023330_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Emails" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "email" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Emails_email_key" ON "Emails"("email");
