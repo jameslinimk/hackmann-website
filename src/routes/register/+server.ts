d87ff406652d18186fe8208036badff2970b48a1
Jameslinimk <jameskimimk@gmail.com>
2024-03-07 23:42:55 -0500

    qwe

--- src/routes/register/+server.ts
+++ src/routes/register/+server.ts
@@ -2,6 +2,7 @@ import { response } from "$lib"
 import { db } from "$lib/db.server.js"
 import { emails } from "$lib/text.json"
 import { validate } from "email-validator"
+import { appendFile } from "fs/promises"
 import type { RequestHandler } from "./$types.js"
 
 export const POST: RequestHandler = async ({ url }) => {
@@ -21,5 +22,9 @@ export const POST: RequestHandler = async ({ url }) => {
 	if (already) return response("Email already registered")
 
 	await db.emails.create({ data: { email, name, school } })
+
+	console.log(`Registered ${email} from ${school} as ${name}`)
+	await appendFile("prisma/log.txt", `${email} from ${school} as ${name}\n`)
+
 	return response()
 }
