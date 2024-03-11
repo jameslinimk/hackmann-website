29c13b440ae6bfbd74024f0b1b44ad340760c181
Linimik <jameskimimk@gmail.com>
2024-03-11 15:12:18 -0400

    qwe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,12 +31,12 @@
 
 <div bind:this={parentDiv} class="text-lg text-blk lg:text-xl px-6 md:px-14 lg:px-40 2xl:px-96 mt-4 lg:mt-8 font-raleway">
 	<p class="mb-4 lg:mb-8">
-		<strong>HackMann</strong> is a virtual hackathon hosted by Horace Mann School Programming Club. Over the course of <strong>Friday, April 5 to Sunday, April 7</strong>, teams of up to two in both
-		the beginner and intermediate-advanced competitions will
-		<strong class="font-semibold text-blk">compete to build a working proof of concept technology that solves an issue based on an a given theme</strong> (given during the opening ceremony). Mentors
-		will be available to assist students in developing their applications, and workshops will be held throughout the weekend to help you learn new skills and get started on your project. At the end,
-		each team will give a presentation of its project, and the judges will decide the winners. If you're with your friends, great! But, if you're coming solo, we'll have time at the beginning of the
-		event to form new teams. Register below to add your email to our list and
+		<strong>HackMann</strong> is a virtual hackathon hosted by the Horace Mann School Programming Club. Over the course of <strong>Friday, April 5 to Sunday, April 7</strong>, teams of up to two in
+		both the beginner and intermediate-advanced competitions will
+		<strong class="font-semibold text-blk">compete to build a working proof of concept technology that solves an issue based on a given theme</strong> (given during the opening ceremony). Mentors will
+		be available to assist students in developing their applications, and workshops will be held throughout the weekend to help you learn new skills and get started on your project. At the end, each
+		team will present their project, and the judges will decide the winners. If you're with your friends, great! But if you're coming solo, we'll have time at the beginning of the event to form new
+		teams. Please register below to add your email to our list and
 		<strong class="font-semibold text-blk"
 			>join our
 			<a href="/discord" class="a">Discord server</a></strong
@@ -46,12 +46,12 @@
 	<div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-2 lg:mb-4">
 		<Faq title="Who can join?">
 			<p>
-				We encourage <strong class="font-semibold text-blk">any middle or high school student</strong> to participate in HackMann. We are open to all skills and skill levels, and we will have workshops
-				and mentors to help you get started. We will have a beginner division for those who are new to coding and an advanced division for those who have experience.
+				We encourage <strong class="font-semibold text-blk">any middle or high school student</strong> to participate in Hackmann. We are open to all skills and skill levels and will have workshops and
+				mentors to help you get started. We will have a beginner division for those new to coding and an advanced division for those with experience.
 			</p>
 		</Faq>
 		<Faq title="What are the prizes?">
-			<p class="mb-1">The awards will be given out based on creativity, usefulness, technical prowess, and concept. The prizes given are as follows:</p>
+			<p class="mb-1">The awards will be based on creativity, usefulness, technical prowess, and concept. The prizes given are as follows:</p>
 			<div>
 				<img src="/first.svg" alt="Trophy" class="inline select-none" />
 				Advanced Division (x2 per team)
@@ -84,7 +84,7 @@
 		<Faq title="What if I don't know how to code?">
 			<p>
 				No problem! We'll have workshops and mentors to help you get started, and beginner teams will be judged separately from advanced teams. We encourage you to learn something new and have fun!
-				Hackathons are a great way to learn to code and learn how to design a project from start to finish.
+				Hackathons are a great way to learn to code and design a project from start to finish. Remember, judges will look at more than code quality, so don't be afraid to join!
 			</p>
 		</Faq>
 	</div>
@@ -111,6 +111,11 @@
 	</div>
 
 	<Divider>Contact Us</Divider>
+
+	<p class="text-center text-sm lg:text-base -mt-3 mb-2 lg:px-6">
+		If you have any questions, feel free to reach out to us at any of the following emails or on our
+		<a href="/discord" class="a">Discord server</a> as admins.
+	</p>
 
 	<div class="flex flex-col justify-center items-center gap-0.5 mb-2">
 		{#each text.emails as email}
